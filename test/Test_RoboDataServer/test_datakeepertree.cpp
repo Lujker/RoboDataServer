5ddc2d21c0bad5607704ac7dcdb22a9893f6e17e
Vlad Zelenskiy <vladzzp111@gmail.com>
2021-03-11 14:41:48 +0300

    create test proj

---
+++ test/Test_RoboDataServer/test_datakeepertree.cpp
@@ -0,0 +1,6 @@
+#include "test_datakeepertree.h"
+
+Test_DataKeeperTree::Test_DataKeeperTree(QObject *parent)
+{
+
+}
---
+++ test/Test_RoboDataServer/test_datakeepertree.h
@@ -0,0 +1,18 @@
+#ifndef TEST_DATAKEEPERTREE_H
+#define TEST_DATAKEEPERTREE_H
+#include <QObject>
+#include "../../include/DataKeeperTree.h"
+
+
+class Test_DataKeeperTree: public QObject
+{
+    Q_OBJECT
+public:
+    explicit  Test_DataKeeperTree(QObject *parent = 0);
+
+private slots:
+    ///Test slots
+
+};
+
+#endif // TEST_DATAKEEPERTREE_H
