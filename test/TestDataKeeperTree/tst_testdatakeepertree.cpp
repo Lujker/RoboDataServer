03bd6fb5fc81f136e982f5164d6dbdcbf89e7d68
Vlad Zelenskiy <vladzzp111@gmail.com>
2021-03-13 01:53:08 +0300

    change test func

--- test/TestDataKeeperTree/tst_testdatakeepertree.cpp
+++ test/TestDataKeeperTree/tst_testdatakeepertree.cpp
@@ -12,28 +12,31 @@ public:
     ~TestDataKeeperTree();
 
 private slots:
-    void test_init_attr();
+    void test_init_attr_data();
 
 };
 
 TestDataKeeperTree::TestDataKeeperTree()
-{
-
-}
+{}
 
 TestDataKeeperTree::~TestDataKeeperTree()
+{}
+
+void TestDataKeeperTree::test_init_attr_data()
 {
+    Attribute attr;
+    Attribute attr_1(new QVariant("1"));
+    Attribute attr_2("name");
+    Attribute attr_3(new QVariant("3"),"Name");
 
-}
+//    QTest::addColumn<bool>("Is root?");
+//    QTest::addColumn<QString>("Value");
+//    QTest::addColumn<QString>("Name");
 
-void TestDataKeeperTree::test_init_attr()
-{
-//    Attribute attr;
-    DataKeeperTree dkt;
-
-//    QTest::addColumn<bool>(attr.is_root());
-    QCOMPARE(dkt.is_root(),true);
-
+//    QTest::newRow("attr")<<attr.is_root()<<((attr.get_val()==nullptr)? "":attr_2.get_val()->toString())<<QString::fromStdString(attr.get_name());
+//    QTest::newRow("attr_1")<<attr_1.is_root()<<((attr_1.get_val()==nullptr)? "":attr_1.get_val()->toString())<<QString::fromStdString(attr_1.get_name());
+//    QTest::newRow("attr_2")<<attr_2.is_root()<< ((attr_2.get_val()==nullptr)? "":attr_2.get_val()->toString())<<QString::fromStdString(attr_2.get_name());
+//    QTest::newRow("attr_3")<<attr_3.is_root()<<((attr_3.get_val()==nullptr)? "":attr_3.get_val()->toString())<<QString::fromStdString(attr_3.get_name());
 }
 
 QTEST_APPLESS_MAIN(TestDataKeeperTree)
